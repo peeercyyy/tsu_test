a883e40affb55869a517bbcae528706f2667d0be
Vyacheslav Bogdanov <slavabogdn@gmail.com>
2023-02-01 20:12:49 +0700

    initial commit

---
+++ src/app/Users/User.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id: number;
+  createDate: string;
+  avatar?: string;
+  firstName: string;
+  lastName: string;
+  patronymic?: string;
+  email: string;
+  about?: string;
+}
